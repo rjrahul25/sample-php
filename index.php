fd3cf639404667a579ef2da59a8319c0f1443da0
rjrahul25 <rjrahul25@gmail.com>
2021-08-30 13:11:46 +0530

    asdasdsad
    hello

--- index.php
+++ index.php
@@ -6,9 +6,7 @@ use Cowsayphp\Farm;
 header('Content-Type: text/plain');
 
 $text = "hello testsdkfksjflksdjflsjdlfjsdlf";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
+
 
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
 echo $cow->say($text);
